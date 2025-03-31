2f7997fc1101a0e7fc47abba82c4150675757789
unknown <thanawat.siriwisitthana@gmail.com>
2025-03-31 14:08:52 +0700

    fix: Improve environment variables handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/reward2/', // ชื่อ repository ของคุณ
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    base: '/reward2/', // ชื่อ repository ของคุณ
+    define: {
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+    },
+  }
 })
