3406d36150ecb591182475617789a39a20b5e9b4
unknown <thanawat.siriwisitthana@gmail.com>
2025-04-05 08:05:53 +0700

    fix: แก้ไข base path ใน vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
-    base: '/ice-reward/',
+    base: '/reward2/',
     define: {
       'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
       'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
